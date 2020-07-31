d8e99e6479a0c8646167a1d0688fb78fcc9506d7
p0ria <p0ria@live.com>
2020-07-31 11:02:30 +0430

    normalize css added

---
+++ src/Pages/Login/LoginPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./LoginPage.scss";
+
+export default (props: any) => {
+  return (
+    <div className="LoginPage">
+      <h1>Login Page</h1>
+    </div>
+  )
+}
