76ff70cc7817c6e778bb617e36b268e2d1df3803
p0ria <p0ria@live.com>
2020-09-13 23:52:17 +0430

    HomePage Logo added

--- src/Pages/Home/HomePage.tsx
+++ src/Pages/Home/HomePage.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import './HomePage.scss';
+import Logo from '../../components/Logo/Logo';
 
 export default () => {
   return (
     <div className="HomePage">
-      Home Page
+      <div className="HomePage-Left">
+        <div className="HomePage-Left__Top">
+          <Logo />
+        </div>
+        
+      </div>
+      <div className="HomePage-Right">
+
+      </div>
     </div>
   )
 }
